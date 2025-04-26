615a68cf6e6477b4ce52dd7ea2f82e334675c6d4
Dominik Fay <dominik.fay@gmx.de>
2025-04-26 19:16:58 +0200

    Add strategies to evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -9,13 +9,13 @@ import numpy as np
 from tqdm import trange
 
 # Add your new class here
-from strategies.n_guess import NGuess
+from strategies.max_clue_entropy import MaxClueEntropy
 
 # Instantiate it at most 3 times with different parameters here
 STRATEGIES_UNDER_TEST = [
-    NGuess(epsilon=7.8, G=3), # Targets 5th percentile
-    NGuess(epsilon=24, G=3), # Targets 50th percentile
-    NGuess(epsilon=34, G=4), # Targets 95th percentile
+    MaxClueEntropy(n_guesses=2, epsilon_per_guess=7.5, monte_carlo=400),  # aim for 5th percentile
+    MaxClueEntropy(n_guesses=3, epsilon_per_guess=13., monte_carlo=200),  # aim for 50th percentile
+    MaxClueEntropy(n_guesses=3, epsilon_per_guess=32., monte_carlo=200),  # aim for 95th percentile
 ]
 
 NUM_TRIALS = 10001
