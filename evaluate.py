c4ff9b30a2f9990e18cb2f0b3909ed7a665892a5
Damien Desfontaines <TedTed@users.noreply.github.com*>
2025-04-30 10:29:19 +0200

    record new high score

--- evaluate.py
+++ evaluate.py
@@ -9,17 +9,18 @@ import numpy as np
 from tqdm import trange
 
 # Add your new class here
+from strategies.n_guess import NGuess
 from strategies.max_clue_entropy import MaxClueEntropy
 
 # Instantiate it at most 3 times with different parameters here
 STRATEGIES_UNDER_TEST = [
-    MaxClueEntropy(n_guesses=2, epsilon_per_guess=7.5, monte_carlo=400),  # aim for 5th percentile
-    MaxClueEntropy(n_guesses=3, epsilon_per_guess=13., monte_carlo=200),  # aim for 50th percentile
-    MaxClueEntropy(n_guesses=3, epsilon_per_guess=32., monte_carlo=200),  # aim for 95th percentile
+    NGuess(epsilon=7.8, G=3), # Current best for 5th percentiel
+    MaxClueEntropy(n_guesses=3, epsilon_per_guess=13., monte_carlo=200),  # Current best for 50th percentile
+    MaxClueEntropy(n_guesses=3, epsilon_per_guess=32., monte_carlo=200),  # Current best for 95th percentile
 ]
 
-NUM_TRIALS = 10001
-TIMEOUT_DURATION = 5
+NUM_TRIALS = 1001
+TIMEOUT_DURATION = 10
 
 with open("valid.txt", "r") as f:
     valid = set(f.read().splitlines())
